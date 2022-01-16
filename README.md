b255c26a407ce60b24871637953f119904ae4a6c
Frontier9 <48641859+MichaelPNolan@users.noreply.github.com>
2022-01-16 18:38:27 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">esp32_esp8266_organ</h1>
+<h3 align="center">ESP32, ESP8266 based MIDI Organ using the ML_SynthTools library (little example arduino project)</h3>  
+<p align="center"> 
+  <img src="img/splash.jpg" alt="project picture" width="480px" height="270px"><br>
+  <a href="https://youtu.be/c7TL8jcrnzs">link to the video</a>
+</p>
+
+The required library can be found here: https://github.com/marcel-licence/ML_SynthTools
+
+Demo using the XIAO: https://youtu.be/2wT8nByoUNw
+Demo using the Teensy4.1: https://youtu.be/H-NDCQnSDV0
+
+more information available in future
---
+++ config.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2021 Marcel Licence
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ * veröffentlichten Version, weiter verteilen und/oder modifizieren.
+ *
+ * Dieses Programm wird in der Hoffnung bereitgestellt, dass es nützlich sein wird, jedoch
+ * OHNE JEDE GEWÄHR,; sogar ohne die implizite
+ * Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Einzelheiten.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file config.h
+ * @author Marcel Licence
+ * @date 21.11.2021
+ *
+ * @brief This file contains the project configuration
+ *
+ * All definitions are visible in the entire project
+ *
+ * Put all your project settings here (defines, numbers, etc.)
+ * configurations which are requiring knowledge of types etc.
+ * shall be placed in z_config.ino (will be included at the end)
+ */
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+#define ESP32_AUDIO_KIT
+//#define AUDIO_KIT_BUTTON_DIGITAL seems to be buggy
+#define AC101_ENABLED
+#define ESP32
+
+#ifdef ESP8266
+
+#define SWAP_SERIAL
+//#define I2S_NODAC
+#define LED_PIN     LED_BUILTIN
+
+#define RXD2 13 /* U2RRXD, D7 */
+#define TXD2 15 /* U2RRXD, D0 */
+#ifndef SWAP_SERIAL
+#include <SoftwareSerial.h>
+SoftwareSerial Serial2(RXD2, TXD2);
+#endif
+
+#endif /* ESP8266 */
+#ifdef ESP32_AUDIO_KIT
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define LED_PIN     22
+#define MIDI_RX_PIN 21
+#else /* ESP32_AUDIO_KIT */
+
+#ifdef ESP32
+#define I2S_BCLK_PIN    25
+#define I2S_WCLK_PIN    27
+#define I2S_DOUT_PIN    26
+
+#define LED_PIN     2
+#include "boards/board_esp32_doit.h"
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define SAMPLE_BUFFER_SIZE  48
+
+//#define MIDI_VIA_USB_ENABLED /* activate this when connected to the USB host breakout board */
+
+#define RXD2 21
+#define TXD2 17
+
+//#define MIDI_RX_PIN RXD2
+
+#endif /* ESP32 */
+#endif /* ESP32_AUDIO_KIT */
+
+
+#ifdef TEENSYDUINO // CORE_TEENSY
+
+#define LED_PIN 13 /* led pin on teensy 4.1 */
+#define MIDI_SERIAL1_BAUDRATE   115200
+
+#endif /* TEENSYDUINO */
+
+#define MIDI_IN RXD2
+#define MIDI_FMT_INT
+#define MIDI_BAUDRATE   115200
+
+
+#endif /* CONFIG_H_ */
